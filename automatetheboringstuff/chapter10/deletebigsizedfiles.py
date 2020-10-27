399767b9cdfdb1bfffc8066db820da7a9ee02f74
skylar01 <realreyes9@gmail.com>
2020-10-27 12:54:39 +0800

    First

---
+++ automatetheboringstuff/chapter10/deletebigsizedfiles.py
@@ -0,0 +1,17 @@
+# "Delete" files that have a file size of more than 100MB.
+import os, shutil
+
+def removefiles(oldir):
+    for folders, subfolders, filenames in os.walk(oldir):
+        for files in filenames:
+            os.chdir(folders)
+            size = os.path.getsize(files)
+            if size >= 100000000:
+                print(os.path.abspath(files))
+                os.remove(files)
+                print("Successfully removed.")
+
+if __name__ == "__main__":
+    os.chdir('')
+    oldir = os.getcwd()
+    removefiles(oldir)
